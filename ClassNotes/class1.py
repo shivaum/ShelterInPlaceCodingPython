ff3487fbb6276684b523a7b66f9d7e8c17ed1519
Shivaum <shivaum1@gmail.com>
2020-04-27 18:24:33 -0700

    class1 notes

---
+++ ClassNotes/class1.py
@@ -0,0 +1,54 @@
+# print("Hello my name is Shivaum")
+
+# name = "Shivaum"
+# age = 19
+# print(name)
+# print(age)
+
+# age = 20
+# print(age)
+
+# pi = 3.14
+# print(pi)
+# pi = 3
+# print(pi)
+
+# year = 2020
+# year_born = 2001
+
+# age = year - year_born
+
+# print(age)
+
+a = 100
+b = 10
+c = 50
+# print(c)
+
+# c = a / b
+# print(c)
+
+# d = a * b
+# print(d)
+
+# b += 5 # b = 15
+# b = b + 5 # b = 20
+# print(b)
+
+# print("Here are my varaibles {}, {}, {}".format(b, a, c))
+
+
+item1 = "apples"
+amount_of_apples = 5
+
+print("Hello! I have some {}. How many would you like? I have {} apples.".format(item1, amount_of_apples))
+
+print("Hello! I have some " + item1 + ". How many would you like? I have " + str(amount_of_apples) + " apples.")
+
+
+
+# print("I have some " + item1)
+# print("I have " + str(amount_of_apples))
+
+
+# # tinyurl.com/ShelterCoding
