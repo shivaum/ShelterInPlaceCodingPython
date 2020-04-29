249703dec67fcc8fabbe69885f5a2c72984c0d22
Caitlyn Chen <caitlynachen@gmail.com>
2020-04-28 18:04:46 -0700

    python

---
+++ ClassNotes/class2.py
@@ -0,0 +1,44 @@
+# zero = 0
+# zero_division = 4 / zero
+# print(zero_division)
+
+#go over camel casing
+#quick hw review
+#review from concepts from last class
+myAge = 19
+yourAge = 25
+
+d = 5
+c = 3
+d *= c
+
+print(d)
+
+a = 10
+b = 3
+print(a % b)
+
+isHappy = True
+
+#can be useful when looking at strings
+e = 'c';
+
+s = "This is a string."
+#explain index in java (0 is first letter)
+new_str = s[10: 16]
+print(new_str)
+
+anotherStr = "Another string"
+s += anotherStr
+
+#indexes start from 0
+#returns index of the first char/string
+#char at only does for chars
+print(s.index("str"))
+#returns int of string length
+print(len(s))
+
+input("Hi my name is: ")
+
+age = input("Enter age: ")
+print("My age is: " + age)
---
+++ HWSols/hw1.py
@@ -0,0 +1,34 @@
+ingredient1 = "blueberries"
+amount1 = 5
+
+ingredient2 = "apples"
+amount2 = 3
+
+ingredient3 = "bananas"
+amount3 = 2.5
+
+print("This is how you make a fruit smoothie.")
+print("Put " + str(amount1) + " " + ingredient1 + " in the blender.")
+print("Put " + str(amount2) + " " + ingredient2 + " in the blender.")
+print("Put " + str(amount3) + " " + ingredient3 + " in the blender.")
+print("Blend!")
+
+amount1 /= 2
+amount2 /= 2
+amount3 /= 2
+
+print("This is how you make half a fruit smoothie.")
+print("Put " + str(amount1) + " " + ingredient1 + " in the blender.")
+print("Put " + str(amount2) + " " + ingredient2 + " in the blender.")
+print("Put " + str(amount3) + " " + ingredient3 + " in the blender.")
+print("Blend!")
+
+amount1 *= 3
+amount2 *= 3
+amount3 *= 3
+
+print("This is how you make 3/2 a fruit smoothie.")
+print("Put " + str(amount1) + " " + ingredient1 + " in the blender.")
+print("Put " + str(amount2) + " " + ingredient2 + " in the blender.")
+print("Put " + str(amount3) + " " + ingredient3 + " in the blender.")
+print("Blend!")
